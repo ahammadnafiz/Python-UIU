26b42ffdee15117be8877b4bd27c49f80c4cf776
ahammadnafiz <86776685+ahammadnafiz@users.noreply.github.com>
2024-11-05 12:48:00 +0600

    Learn about some sorting algo

--- DSA I/bubble_sort.py
+++ DSA I/bubble_sort.py
@@ -1,14 +1,4 @@
 # bubble_sort.py
-def even_odd(arr):
-    even = []
-    odd = []
-    for i in arr:
-        if i %2 ==0:
-            even.append(i)
-        else:
-            odd.append(i)
-    return even, odd
-
 
 def bubble_sort(arr):
     for _ in range(1, len(arr)):
@@ -19,6 +9,4 @@ def bubble_sort(arr):
     return arr
 
 arr = [5, 2, 4, 6, 1, 3]
-even, odd = even_odd(arr)
-print(bubble_sort(even))
-print(bubble_sort(odd))
+print(bubble_sort(arr))
