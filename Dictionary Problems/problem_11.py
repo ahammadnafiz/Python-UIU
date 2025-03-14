2ce2e565112d1aea958378d6f631b8a9ba4e07d9
ahammadnafiz <ahammadnafiz@outlook.com>
2025-03-14 23:52:54 +0600

    Add dictionary problem solutions for character mapping and value analysis

---
+++ Dictionary Problems/problem_11.py
@@ -0,0 +1,9 @@
+dic = {
+    "Iron Man": "Tony Stark",
+    "Captain America": "Steve Rogers",
+    "Thor": "God of Thunder",
+    "Hulk": "Bruce Banner",
+}
+
+for key, value in dic.items():
+    print(f"{key}: {value}")
---
+++ Dictionary Problems/problem_12.py
@@ -0,0 +1,5 @@
+dic = {"x": 500, "y": 5874, "z": 560}
+
+values = [val for val in dic.values()]
+
+print(f"Maximum Vlaue: {max(values)}\nMinimum Vlaue: {min(values)}")
